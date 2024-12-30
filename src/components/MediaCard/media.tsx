20dfb19c63d14441e7dc4e870eb109d2476fafe7
YannGabriel <yanngabrieldev@gmail.com>
2024-12-29 23:07:28 -0300

    create first api medias contact

--- src/components/MediaCard/media.tsx
+++ src/components/MediaCard/media.tsx
@@ -1,15 +1,20 @@
-import github from "../../images/mediaIcons/github.svg";
-import whatsapp from "../../images/whatsapp.svg";
-import linkedin from "../../images/mediaIcons/Linkedin.png";
-import mail from "../../images/mediaIcons/mail.svg";
+
+
+import { useState, useEffect } from "react"
+
+interface Medias{
+  name?: string
+  id?: number,
+  link?: string
+}
 
 const MediaGallery = () =>{
-  const imagens = [
-    {id: 1, src: github, alt: 'github', link: 'https://github.com/YannGabriel'},
-    {id: 2, src: whatsapp, alt: 'whatsapp', link: "https://wa.me/5511993125933?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20seus%20serviços."},
-    {id: 3, src: linkedin, alt: 'linkedin', link: "https://www.linkedin.com/in/yann-gabriel-dev"},
-    {id: 4, src: mail, alt: 'mail', link: "mailto:yanngabrieldev@gmail.com?subject=Primeiro%20Contato&body=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20seus%20serviços."}
-  ]
+
+  const [medias, setMedias] = useState<Medias[]>([])
+
+  useEffect(() =>{
+    fetch("")
+  },[])
 
   return (
     <div className="mediaContent gridLayout">
