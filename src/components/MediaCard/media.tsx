870e17b9334227b89cb7a4f33a1ff23e6db2505d
YannGabriel <yanngabrieldev@gmail.com>
2024-12-29 23:19:52 -0300

    remove db, remake a old thing isn`t a good idea :(

--- src/components/MediaCard/media.tsx
+++ src/components/MediaCard/media.tsx
@@ -1,20 +1,15 @@
-
-
-import { useState, useEffect } from "react"
-
-interface Medias{
-  name?: string
-  id?: number,
-  link?: string
-}
+import github from "../../images/mediaIcons/github.svg";
+import whatsapp from "../../images/whatsapp.svg";
+import linkedin from "../../images/mediaIcons/Linkedin.png";
+import mail from "../../images/mediaIcons/mail.svg";
 
 const MediaGallery = () =>{
-
-  const [medias, setMedias] = useState<Medias[]>([])
-
-  useEffect(() =>{
-    fetch("")
-  },[])
+  const imagens = [
+    {id: 1, src: github, alt: 'github', link: 'https://github.com/YannGabriel'},
+    {id: 2, src: whatsapp, alt: 'whatsapp', link: "https://wa.me/5511993125933?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20seus%20serviços."},
+    {id: 3, src: linkedin, alt: 'linkedin', link: "https://www.linkedin.com/in/yann-gabriel-dev"},
+    {id: 4, src: mail, alt: 'mail', link: "mailto:yanngabrieldev@gmail.com?subject=Primeiro%20Contato&body=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20seus%20serviços."}
+  ]
 
   return (
     <div className="mediaContent gridLayout">
